03239addcd7e05961b4cd7f3d012cd145e9d91f8
AnnonyA <79977221+AnnonyA@users.noreply.github.com>
2026-08-24 23:11:27 -0300

    fix: type babel generator interop

--- src/parser/generate.ts
+++ src/parser/generate.ts
@@ -1,5 +1,19 @@
-import generate from "@babel/generator";
+import generatorModule from "@babel/generator";
 import type * as t from "@babel/types";
+
+interface GeneratorOptions {
+  comments?: boolean;
+  compact?: boolean;
+  retainLines?: boolean;
+}
+
+interface GeneratorOutput {
+  code: string;
+}
+
+type GenerateFunction = (ast: t.Node, options?: GeneratorOptions) => GeneratorOutput;
+
+const generate = generatorModule as unknown as GenerateFunction;
 
 export function generateJavaScript(ast: t.File): string {
   return `${generate(ast, {
